68e6b4ebf8ec18ea6e3b8a62fde22e6ba7681fed
Nehal Narvekar <sahilnarverkar8@gmail.com>
2024-01-28 18:33:00 +0530

    Create SignUp api and Route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express  from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
+
 
 
 
@@ -18,12 +20,14 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000,()=>{
     console.log('Server is Running on port 3000');
 });
 
 
 app.use('/api/user',userRoutes);
-
+app.use('/api/auth',authRoutes);
 
 
