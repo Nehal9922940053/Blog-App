9aedfb0a04c61579214ba59a553da18fb943ad9a
Nehal Narvekar <sahilnarverkar8@gmail.com>
2024-02-01 18:40:57 +0530

    Add Redux-Persist

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Link , useNavigate} from 'react-router-dom';
 import {Spinner, Alert, Button, Label, TextInput } from 'flowbite-react';
 import { useDispatch , useSelector} from 'react-redux';
-import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
+import { signInStart, signInSuccess, signInFailure } from '../store/user/userSlice';
 
 export default function SignIn() {
 
