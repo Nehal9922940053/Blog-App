390c8566d0a82ead8706bf742ea6ffb2dbeee992
Nehal Narvekar <sahilnarverkar8@gmail.com>
2024-03-03 17:32:09 +0530

    Add Signout Functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,7 +5,7 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from  'firebase/s
 import {app} from '../firebase';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure,deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../store/user/userSlice.js';
+import { updateStart, updateSuccess, updateFailure,deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../store/user/userSlice.js';
 import { useDispatch } from 'react-redux';
 import { HiOutlineExclamationCircle } from 'react-icons/hi';
 
@@ -139,6 +139,22 @@ const handleDeleteUser = async() => {
   }
 };
 
+const handleSignout = async() => {
+  try {
+    const res = await fetch('/api/user/signout', {
+      method: 'POST',
+    });
+    const data = await res.json();
+    if(!res.ok) {
+      console.log(data.message);
+    }else {
+      dispatch(signoutSuccess());
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
@@ -192,7 +208,7 @@ const handleDeleteUser = async() => {
     </form>
     <div className='text-red-500 flex justify-between mt-5'>
     <span onClick= {() => setShowModal(true)} className='cursor-pointer'>Delete Account</span>
-    <span className='cursor-pointer'>Sign Out</span>
+    <span className='cursor-pointer' onClick={handleSignout}>Sign Out</span>
     </div>
     
     {updateUserSuccess && (
